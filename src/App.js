0a0de1c94e36d549bdad53923a44bab17a7e0f3c
Beto <jrcampos82@gmail.com>
2023-08-03 20:46:17 -0400

    aula1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable jsx-a11y/anchor-has-content */
+/* eslint-disable no-useless-constructor */
+import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    // this.handleClick = this.handleClick.bind(this)
+
+    this.state = {
+      name: "Beto",
+      counter: 0,
+    };
+  }
+
+  handleClick = () => {
+    const { name } = this.state;
+    console.log(`cliquei ${name}`);
+    this.setState({ name: "Nome mudou para Ryan" });
+  };
+
+  handleClickC = () => {
+    const { counter } = this.state;
+    this.setState({ counter: counter + 1 });
+  };
+
+  render() {
+    const { name, counter } = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            <span onClick={this.handleClick}>
+              {name} - {counter}
+            </span>
+            <br></br>
+            <button onClick={this.handleClickC}>+</button>
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          ></a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
