76813c5b96005500d749d4505577a64a723e9868
Beto <jrcampos82@gmail.com>
2023-08-10 19:25:37 -0400

    classe com estado v2

--- src/App.js
+++ src/App.js
@@ -1,32 +1,44 @@
 /* eslint-disable jsx-a11y/anchor-has-content */
 /* eslint-disable no-useless-constructor */
-import logo from "./logo.svg";
 import "./App.css";
 import { Component } from "react";
 
 class App extends Component {
   state = {
-    posts: [
-      {
-        id: 1,
-        title: "Título 1",
-        body: "Corpo 1",
-      },
-      {
-        id: 2,
-        title: "Título 2",
-        body: "Corpo 2",
-      },
-      {
-        id: 3,
-        title: "Título 3",
-        body: "Corpo 3",
-      },
-    ],
+    posts: [],
   };
 
+  // fetch aqui
+  componentDidMount() {
+    
+    setTimeout(() => {
+      this.setState({
+        counter: 0,
+        posts: [
+          {
+            id: 1,
+            title: "Título 1",
+            body: "Corpo 1",
+          },
+          {
+            id: 2,
+            title: "Título 2",
+            body: "Corpo 2",
+          },
+          {
+            id: 3,
+            title: "Título 3",
+            body: "Corpo 3",
+          },
+        ],
+      });
+    }, 2000)
+
+    
+  }
+
   render() {
-    const { posts } = this.state;
+    const { posts, counter } = this.state;
 
     return (
       <div className="App">
