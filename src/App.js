bee05f7be51a034ef6e905da35e456b96da54c75
Beto <jrcampos82@gmail.com>
2023-08-10 19:56:53 -0400

    update component

--- src/App.js
+++ src/App.js
@@ -4,44 +4,61 @@ import "./App.css";
 import { Component } from "react";
 
 class App extends Component {
+
+  timeoutUpdate = null
+
   state = {
-    posts: [],
+    counter: 0,
+    posts: [
+      {
+        id: 1,
+        title: "Título 1",
+        body: "Corpo 1",
+      },
+      {
+        id: 2,
+        title: "Título 2",
+        body: "Corpo 2",
+      },
+      {
+        id: 3,
+        title: "Título 3",
+        body: "Corpo 3",
+      },
+    ],
   };
 
   // fetch aqui
   componentDidMount() {
-    
-    setTimeout(() => {
-      this.setState({
-        counter: 0,
-        posts: [
-          {
-            id: 1,
-            title: "Título 1",
-            body: "Corpo 1",
-          },
-          {
-            id: 2,
-            title: "Título 2",
-            body: "Corpo 2",
-          },
-          {
-            id: 3,
-            title: "Título 3",
-            body: "Corpo 3",
-          },
-        ],
-      });
-    }, 2000)
-
-    
+    this.handleTimeOut();
   }
+
+  componentDidUpdate() {
+    this.handleTimeOut();
+  }
+
+  componentWillUnmount(){
+    clearTimeout(this.timeoutUpdate)
+  }
+
+
+  handleTimeOut = () => {
+    const { posts, counter } = this.state;
+    this.timeoutUpdate = setTimeout(() => {
+      posts[0].title = "O título mudou...";
+      this.setState({
+        posts,
+        counter: counter + 1,
+      });
+    }, 2000);
+  };
 
   render() {
     const { posts, counter } = this.state;
 
     return (
       <div className="App">
+        <h1>{counter}</h1>
         {posts.map((post) => (
           <div key={post.id}>
             <h1>{post.title}</h1>
